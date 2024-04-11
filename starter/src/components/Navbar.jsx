06f1c99b53d8c1fdd3424ce5d1c5837b5ac63b92
esra <esrabaskaya84@gmail.com>
2024-04-11 11:45:06 +0200

    authSlice

--- starter/src/components/Navbar.jsx
+++ starter/src/components/Navbar.jsx
@@ -10,79 +10,89 @@ import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 
 function ResponsiveAppBar() {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
-  return (
-    <AppBar
-      position="static"
-      color="morcivert"
-      sx={{ backgroundColor: "morcivert.dark" }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <BusinessIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
-          <Typography
-            variant="h6"
-            noWrap
-            component="a"
-            href="/"
-            sx={{
-              mr: 2,
-              display: { xs: "none", md: "flex" },
-              fontFamily: "monospace",
-              fontWeight: 700,
-              letterSpacing: ".3rem",
-              color: "inherit",
-              textDecoration: "none",
-              flexGrow: 1,
-              cursor: "pointer",
-            }}
-          >
-            Clarusway - DE08
-          </Typography>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar>A</Avatar>
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              <MenuItem>
-                <Typography textAlign="center">Login</Typography>
-              </MenuItem>
-              <MenuItem>
-                <Typography textAlign="center">Logout</Typography>
-              </MenuItem>
-            </Menu>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
+	return (
+		<AppBar
+			position="static"
+			color="morcivert"
+			sx={{ backgroundColor: "morcivert.dark" }}
+		>
+			<Container maxWidth="xl">
+				<Toolbar disableGutters>
+					<BusinessIcon
+						sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+					/>
+					<Typography
+						variant="h6"
+						noWrap
+						component="a"
+						href="/"
+						sx={{
+							mr: 2,
+							display: { xs: "none", md: "flex" },
+							fontFamily: "monospace",
+							fontWeight: 700,
+							letterSpacing: ".3rem",
+							color: "inherit",
+							textDecoration: "none",
+							flexGrow: 1,
+							cursor: "pointer",
+						}}
+					>
+						Clarusway - DE08
+					</Typography>
+					<Box sx={{ flexGrow: 0 }}>
+						<Tooltip title="Open settings">
+							<IconButton
+								onClick={handleOpenUserMenu}
+								sx={{ p: 0 }}
+							>
+								<Avatar>A</Avatar>
+							</IconButton>
+						</Tooltip>
+						<Menu
+							sx={{ mt: "45px" }}
+							id="menu-appbar"
+							anchorEl={anchorElUser}
+							anchorOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: "top",
+								horizontal: "right",
+							}}
+							open={Boolean(anchorElUser)}
+							onClose={handleCloseUserMenu}
+						>
+							<MenuItem>
+								<Typography textAlign="center">
+									Login
+								</Typography>
+							</MenuItem>
+							<MenuItem>
+								<Typography textAlign="center">
+									Logout
+								</Typography>
+							</MenuItem>
+						</Menu>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 }
 export default ResponsiveAppBar;
--- starter/src/main.jsx
+++ starter/src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { store } from "./app/store";
+import { Provider } from "react-redux";
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
